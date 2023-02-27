82b89b076dada7b3c132d49aeec20aa20d034972
PhuHuy123 <nguyenphuhuy180920@gmail.com>
2023-02-27 11:44:49 +0700

    fix_login

--- src/App.js
+++ src/App.js
@@ -5,25 +5,26 @@ import { Routes, Route } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getCheckToken } from "./redux/login/actions";
 
-
 const App = () => {
-  const isLogin=localStorage.getItem('access_token')
+  const { isLoggedIn } = useSelector((state) => state.login);
+  const [isLogin, setIsLogin] = useState(localStorage.getItem("access_token"));
   const dispatch = useDispatch();
-  const checkToken = useCallback(
-    () => dispatch(getCheckToken()),
-    [dispatch]
-  );
+  const checkToken = useCallback(() => dispatch(getCheckToken()), [dispatch]);
   useEffect(() => {
-    checkToken()
+    checkToken();
   }, []);
-
+  useEffect(() => {
+    if (isLoggedIn) {
+      setIsLogin(localStorage.getItem("access_token"));
+    }
+  }, [isLoggedIn]);
   return (
     <>
       <Routes>
-        {isLogin && <Route element={<Client />} path="animal/*" />}
+        {isLogin !== null && <Route element={<Client />} path="animal/*" />}
         <Route element={<Auth />} path="auth/*" />
         <Route element={<LoginPage />} path="/*" />
       </Routes>
